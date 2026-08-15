9f3ac1608212da7052b61230e1deb0dc5b6d28d1
pasi <pasiabhishek6070@gmail.com>
2026-08-15 15:22:03 +0530

    working on music section

--- frontend/src/component/Music.jsx
+++ frontend/src/component/Music.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import "./css/Music.css"
 
 export default function Music() {
-  return (
-    <div className="Music" id='Music'>
-       <h1>Books</h1>
-      <p>Words that speak to the soul.</p>
+    return (
+        <div className="Music" id='Music'>
+            <div className='section'>
+                <h1>Music</h1>
+                <p>Melodies that tells stories.</p>
+                <div className='music_player'>
+                    <div className='player'>
+                        <img src="images\Artwork\aagaz-artwork.jpg" width='250' alt="" />
+                        <div className='controls'>
+                            <h3 className='Song_name'>
+                                Aagaz
+                            </h3>
+                            <p className='Song_Artist'>
+                                Master Aazam
+                            </p>
+                            <input type="range" name="play_bar" id="Play_bar" />
+                        </div>
+                    </div>
+                    <div className='playlist'>
+                        <ul>
+                            <li>oggy</li>
+                            <li>oggy</li>
+                            <li>oggy</li>
+                            <li>oggy</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-    </div>
-  )
+
+
+        </div>
+    )
 }
