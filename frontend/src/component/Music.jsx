fbcc256c3b2fcb69b43729f16b70216a87e2e931
pasi <pasiabhishek6070@gmail.com>
2026-08-16 02:23:57 +0530

    music player updated

--- frontend/src/component/Music.jsx
+++ frontend/src/component/Music.jsx
@@ -1,45 +1,129 @@
-import React, { useState } from 'react'
-import "./css/Music.css"
-import Songs from './data/Songs.json'
+import React, { useRef, useState } from "react";
+import "./css/Music.css";
+import Songs from "./data/Songs.json";
 
 export default function Music() {
-    const [varSong, setSong]= useState("Aagaz")
+    const [currentSong, setCurrentSong] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const audioRef = useRef(null);
+    const song = Songs[currentSong];
+
+    const play = () => {
+        audioRef.current.play();
+        setIsPlaying(true);
+    };
+
+    const pause = () => {
+        audioRef.current.pause();
+        setIsPlaying(false);
+    };
+
+    const previous = () => {
+        const index =
+            currentSong === 0 ? Songs.length - 1 : currentSong - 1;
+
+        setCurrentSong(index);
+        setIsPlaying(true);
+
+        setTimeout(() => {
+            audioRef.current.play();
+        }, 0);
+    };
+
+    const next = () => {
+        const index = currentSong === Songs.length - 1 ? 0 : currentSong + 1;
+
+        setCurrentSong(index);
+        setIsPlaying(true);
+
+        setTimeout(() => {
+            audioRef.current.play();
+        }, 0);
+    };
+
+    const selectSong = (index) => {
+        setCurrentSong(index);
+        setIsPlaying(true);
+
+        setTimeout(() => {
+            audioRef.current.play();
+        }, 0);
+    };
+
     return (
-        <div className="Music" id='Music'>
-            <div className='section'>
+        <div className="Music" id="Music">
+            <div className="section">
                 <h1>Music</h1>
                 <p>Melodies that tells stories.</p>
-                <div className='music_player'>
-                    <div className='player'>
-                        <img src="images\Artwork\aagaz-artwork.jpg" width='250' alt="" />
-                        <div className='controls'>
-                            <h3 className='Song_name'>
-                                {varSong}
+                <div className="music_player">
+                    <div className="player">
+                        <img  src={song.artwork} width="250" alt={song.title}/>
+
+                        <div className="controls">
+                            <h3 className="Song_name">
+                                {song.title}
                             </h3>
-                            <p className='Song_Artist'>
-                                Master Aazam
+                            <p className="Song_Artist">
+                                {song.artist}
                             </p>
-                            <input type="range" name="play_bar" id="Play_bar" />
+                            <audio ref={audioRef} src={song.audio} onEnded={next } />
+                            <input type="range" name="play_bar" id="Play_bar" min="0" max="100" defaultValue="0"  />
+
+                            <div className="control_button">
+                                <div className="song_controls">
+                                    <ul>
+                                        <li onClick={previous}>
+                                            <i className="fa-solid fa-backward-step"></i>
+                                        </li>
+                                        <li onClick={isPlaying ? pause: play}>
+                                            <i className={isPlaying? "fa-solid fa-pause": "fa-solid fa-play"}></i>
+                                        </li>
+
+                                        {/* Next */}
+                                        <li onClick={next}>
+                                            <i className="fa-solid fa-forward-step"></i>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div className='playlist'>
-                        {
-                            Songs.map((song) => (
 
-                                <div className='playlist-row' key={song.id} onClick={()=>setSong(song.title)}>
-                                    <span className='song_id'>{song.id}</span>
-                                    <span className='song_title'>{song.title}</span>
-                                    <span className='song_duration'>{song.duration}</span>
-                                </div>
+                    {/* PLAYLIST */}
+                    <div className="playlist">
 
-                            ))
-                        }
+                        {Songs.map((song, index) => (
+                            <div
+                                className={
+                                    currentSong === index
+                                        ? "playlist-row active"
+                                        : "playlist-row"
+                                }
+                                key={song.id}
+                                onClick={() => selectSong(index)}
+                            >
+
+                                <span className="song_id">
+                                    {song.id}
+                                </span>
+
+                                <span className="song_title">
+                                    {song.title}
+                                </span>
+
+                                <span className="song_duration">
+                                    {song.duration}
+                                </span>
+
+                            </div>
+                        ))}
+
                     </div>
+
                 </div>
+
             </div>
 
-
-
         </div>
-    )
+    );
 }
