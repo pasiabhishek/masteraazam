9b7a7a9bf43fef6fd49f469d61d26a5a1cb0990a
pasi <pasiabhishek6070@gmail.com>
2026-08-15 14:16:11 +0530

    css reordered

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import Nav from './component/Nav'
 import Hero from './component/Hero'
 import About from './component/About'
 import Book from './component/Book'
+import Music from './component/Music'
 import './App.css'
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Hero />
       <About />
       <Book />
+      <Music />
     </>
   )
 }
---
+++ frontend/src/component/Music.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Music() {
+  return (
+    <div className="Music" id='Music'>
+       <h1>Books</h1>
+      <p>Words that speak to the soul.</p>
+
+    </div>
+  )
+}
